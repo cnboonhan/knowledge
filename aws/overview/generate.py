8dfb13178ad5dee1b17e17c6eebf7d13c708c4c3
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-05-06 05:54:31 +0000

    add more services

--- aws/overview/generate.py
+++ aws/overview/generate.py
@@ -2,10 +2,10 @@ from lib import generate_aws_phrases, generate_aws_prompts
 import boto3
 import json
 
-for client in ['rds', 'apigatewayv2', 'ec2']:
+for client in ['rds', 'apigatewayv2', 'ec2', 'codedeploy', 'codebuild' ,'codepipeline', 'elasticbeanstalk', 'opsworks', 'cloudformation', 'kinesis', 'cloudtrail', 'cloudwatch', 'iam', 'appconfig', 'config', 'guardduty', 'inspector', 'ssm', 'kms', 'autoscaling']:
     phrases = generate_aws_phrases(boto3.client(client, region_name='ap-southeast-1'))
     prompts = generate_aws_prompts(phrases, client)
 
     with open(f"{client}.json", "w") as outfile:
-        json.dump({'client': client, 'data': phrases, 'prompts': prompts}, outfile, indent=4)
+        json.dump({'client': client, 'data': phrases}, outfile, indent=4)
         print(f"generated for client {client}")
